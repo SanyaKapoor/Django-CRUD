3aa0dd55a5d3dc46a7edfc86ea6985712c98d1b9
SanyaKapoor <sanyakapoor0999@gmail.com>
2020-10-31 17:25:08 +0530

    Added CRUD operations

--- crudexample/__init__.py
+++
@@ -1,3 +0,0 @@
-#!pip install pymysql
-import pymysql
-#pymysql.install_as_MySQLdb()
--- crudexample/settings.py
+++ crudexample/settings.py
@@ -73,16 +73,14 @@ WSGI_APPLICATION = 'crudexample.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'djangodb',  
-        'USER':'',  
-        'PASSWORD':'',  
-        'HOST':'',  
-        'PORT':'3306'  
-    }      
-}  
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+ 
 
 
 # Password validation
